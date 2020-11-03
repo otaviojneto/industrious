0acf7c1c676cd0d798e1409ca5b94d4a5aea6216
Otavio Neto <otavionetosistema@gmail.com>
2020-11-03 19:53:24 -0300

    Updates

---
+++ precache-manifest.a6dd0153e702ae8c22d7e94daa98de7e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510e0e0b92a0ad6ca6d64d1b945812a8",
+    "url": "/industrious/index.html"
+  },
+  {
+    "revision": "18fa34aeffc2d6cd674a",
+    "url": "/industrious/static/css/main.ff1f26e2.chunk.css"
+  },
+  {
+    "revision": "54a7165dbf0c1130a9c2",
+    "url": "/industrious/static/js/2.e55ab6f2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/industrious/static/js/2.e55ab6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18fa34aeffc2d6cd674a",
+    "url": "/industrious/static/js/main.ac21ea5b.chunk.js"
+  },
+  {
+    "revision": "a30c62b7fc1f0948c98f",
+    "url": "/industrious/static/js/runtime-main.5f3dd53d.js"
+  },
+  {
+    "revision": "b69e958fbfcbeb398fba17001b9d52ba",
+    "url": "/industrious/static/media/bg.b69e958f.jpg"
+  },
+  {
+    "revision": "110272caf82625aca861cb1b4625c0d5",
+    "url": "/industrious/static/media/cta01.110272ca.jpg"
+  },
+  {
+    "revision": "20330989617d0cc2cee53d2a221c2174",
+    "url": "/industrious/static/media/generic.20330989.jpg"
+  },
+  {
+    "revision": "2e72e89ab86e53a38dfc02856355c969",
+    "url": "/industrious/static/media/pic01.2e72e89a.jpg"
+  },
+  {
+    "revision": "80a19663f41eadc2f97d502b4eaf94bd",
+    "url": "/industrious/static/media/pic02.80a19663.jpg"
+  },
+  {
+    "revision": "5096fe30b76119a85313bd0e8b0c4a1c",
+    "url": "/industrious/static/media/pic03.5096fe30.jpg"
+  },
+  {
+    "revision": "a6a905c4c9d0d5810bc29330a90aa4b9",
+    "url": "/industrious/static/media/undraw_page_not_found_su7k.a6a905c4.svg"
+  },
+  {
+    "revision": "7861dadc7807c489531e672f28d0ecec",
+    "url": "/industrious/static/media/user-default.7861dadc.png"
+  }
+]);
